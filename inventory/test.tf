7e8e67d02a59ed87baf36b8dc609fa2c3856ac5b
mkol5222 <mkoldov@gmail.com>
2025-05-30 12:21:07 +0000

    .

---
+++ inventory/test.tf
@@ -0,0 +1,13 @@
+
+# IPv6_Link_Local_Hosts
+
+# resource "checkpoint_management_network" "networks" {
+
+
+
+#   name         = "IPv6_Link_Local_Hosts"
+#   subnet4 = "1.2.3.4"
+#   mask_length4  = 24
+
+  
+# }
